fb92fe1d56b87b73720659c4b2505d9cb415ddbf
LILBHAHAHAHHAHAHAAHAHHHAHAHAHAAH <deception@fbi.al>
2013-09-04 22:12:35 -0700

    Update survival_mode.cpp

--- survival_mode.cpp
+++ survival_mode.cpp
@@ -1,13 +1,25 @@
 #include "ScriptPCH.h"
 
+
+#define MAX_PLAYERS		5
+
 class survival_modes : public CreatureScript
 {
 public:
 	survival_modes() : CreatureScript("survival_mode") {}
-
+	std::list<Unit *> list;
 	bool OnGossipHello(Player* player, Creature* creature)
 	{
-		
+		 player->GetPartyMembers(list);
+		 if (list.max_size() == MAX_PLAYERS)
+		 {
+			 if (player->GetCharmerOrOwnerGUID == player)
+			 {
+				 player->ADD_GOSSIP_ITEM();
+			 }
+		 }
+
+
 	}
 
 	bool OnGossipSelect(Player* player, Creature* creature, uint32 sender, uint32 action)
@@ -15,7 +27,6 @@ public:
 
 	}
 };
-
 void AddSC_survival_mode()
 {
 	new survival_modes;
