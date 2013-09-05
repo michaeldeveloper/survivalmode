e0834d6d953cd351724776b842f17dbddb4a2fda
michaeldeveloper <mikey92394@yahoo.com>
2013-09-04 23:57:13 -0500

    survival start

---
+++ survival_mode.cpp
@@ -0,0 +1,20 @@
+#include "ScriptPCH.h"
+
+class survival_modes : public CreatureScript
+{
+	survival_modes() : CreatureScript("survival_mode") {}
+
+	bool OnGossipHello(Player* player, Creature* creature)
+	{
+		
+	}
+
+	bool OnGossipSelect(Player* player, Creature* creature, uint32 sender, uint32 action)
+	{
+
+	}
+};
+void AddSC_survival_mode()
+{
+	new survival_modes;
+}
