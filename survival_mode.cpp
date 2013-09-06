6053984cda8b5d890373ecfd5df4a73b0f310b6d
michaeldeveloper <mikey92394@yahoo.com>
2013-09-05 23:01:31 -0500

    script edit, along with group.cpp

--- survival_mode.cpp
+++ survival_mode.cpp
@@ -1,17 +1,36 @@
 #include "ScriptPCH.h"
 
+#define MAX_PLAYERS		2
+
 class survival_modes : public CreatureScript
 {
+public:
 	survival_modes() : CreatureScript("survival_mode") {}
-
+	std::list<Unit *> list;
 	bool OnGossipHello(Player* player, Creature* creature)
 	{
-		
+		creature->MonsterYell("WHY YOU SPEAK?!", LANG_UNIVERSAL, 0);
+		player->GetPartyMembers(list);
+		if (list.size() == MAX_PLAYERS)
+		{
+			creature->MonsterSay("You have right amount of players, of 2.", LANG_UNIVERSAL, 0);
+			uint64 player_guid = player->GetGUID();	
+			if (player->HasFlag(PLAYER_FLAGS, PLAYER_FLAGS_GROUP_LEADER))
+			{
+				creature->MonsterSay("Gay leader you have.", LANG_UNIVERSAL, 0);
+				player->ADD_GOSSIP_ITEM(GOSSIP_ICON_BATTLE, "Survival Game", GOSSIP_SENDER_MAIN, 0);
+				player->ADD_GOSSIP_ITEM(GOSSIP_ICON_INTERACT_1, "Nevermind", GOSSIP_SENDER_MAIN, 1);
+			}
+		}
+		player->SEND_GOSSIP_MENU(1, creature->GetGUID());
+
+		list.clear();
+		return true;
 	}
 
 	bool OnGossipSelect(Player* player, Creature* creature, uint32 sender, uint32 action)
 	{
-
+		return true;
 	}
 };
 void AddSC_survival_mode()
